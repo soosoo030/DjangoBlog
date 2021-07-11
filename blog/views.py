12203a15de74de7af9a1c77b2ed779c069dc8ca9
soosoo030 <whereru511@gmail.com>
2021-07-11 12:16:58 +0900

    user확장 및 paginator

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,21 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.utils import timezone
+from django.core.paginator import Paginator
 from .models import Blog
+from .forms import BlogForm
+
 # Create your views here.
 def home(request):
-    blogs = Blog.objects.all()
+    blogs = Blog.objects.order_by('-pub_date')
+    search = request.GET.get('search')
+    if search == 'true':
+        author = request.GET.get('writer')
+        blogs = Blog.objects.filter(writer=author).order_by('-pub_date')
+        return render(request,'home.html',{'blogs':blogs})
+
+    paginator = Paginator(blogs, 8)
+    page = request.GET.get('page')
+    blogs = paginator.get_page(page)
     return render(request, 'home.html',{'blogs':blogs})
 
 def detail(request, id):
@@ -11,16 +23,24 @@ def detail(request, id):
     return render(request, 'detail.html', {'blog':blog})
 
 def new(request):
-    return render(request, 'new.html')
+    form = BlogForm()
+    return render(request, 'new.html',{'form':form})
 
 def create(request):
-    new_blog = Blog()
-    new_blog.title = request.POST['title']
-    new_blog.writer = request.POST['writer']
-    new_blog.body = request.POST['body']
-    new_blog.pub_date = timezone.now()
-    new_blog.save()
-    return redirect('detail', new_blog.id)
+    form = BlogForm(request.POST, request.FILES)
+    if form.is_valid():
+        new_blog = form.save(commit=False)
+        new_blog.pub_date = timezone.now()
+        new_blog.save()
+    # new_blog = Blog()
+    # new_blog.title = request.POST['title']
+    # new_blog.writer = request.POST['writer']
+    # new_blog.body = request.POST['body']
+    # new_blog.pub_date = timezone.now()
+    # new_blog.image = request.FILES['image']
+    # new_blog.save()
+        return redirect('detail', new_blog.id)
+    return redirect('home')
 
 def edit(request, id):
     edit_blog = Blog.objects.get(id= id)
@@ -39,3 +59,4 @@ def delete(request, id):
     delete_blog = Blog.objects.get(id= id)
     delete_blog.delete()
     return redirect('home')
+
--- lionproject/settings.py
+++ lionproject/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -28,6 +28,8 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 
+AUTH_USER_MODEL = 'account.CustomUser'
+
 # Application definition
 
 INSTALLED_APPS = [
@@ -38,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog',
+    'account',
 ]
 
 MIDDLEWARE = [
@@ -55,7 +58,7 @@ ROOT_URLCONF = 'lionproject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['lionproject/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,7 +123,15 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = [ os.path.join(BASE_DIR, 'blog', 'static')
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
